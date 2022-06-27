90112d94b25437b781cea257b19f5ba0546b2eb6
anaBeatrizLM <101081223+anaBeatrizLM@users.noreply.github.com>
2022-06-26 21:03:41 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ComoFuncionaUsuario from './Pages/Como_funciona_usuario/Como_funciona_usuario';
+import ComoFuncionaMei from './Pages/Como_funciona_mei/Como_funciona_mei';
+import Servicos from './Pages/Servicos/Servicos';
+import QuemSomos from './Pages/Quem_somos/Quem_somos';
+import './App.css';
+
+import {BrowserRouter as Router, Switch,Route,Link} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+    <QuemSomos />
+    </>
+  );
+}
+
+export default App;
